4eacaee930eab0bd4f8f53c05ef72795216ad5f1
summer <wpsumsun@126.com>
2017-04-24 23:10:08 +0800

    import jquery

--- webpack-demo/app/index.js
+++ webpack-demo/app/index.js
@@ -1,11 +1,11 @@
 import _ from 'lodash'
+import $ from 'jquery'
 function component () {
-  var element = document.createElement('div');
+  var element=$('<div>/div>');
 
   /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-  return element;
+  element.html(_.join(['Hello','webpack'],''));
+  return element.get(0);
 }
 
 document.body.appendChild(component());
